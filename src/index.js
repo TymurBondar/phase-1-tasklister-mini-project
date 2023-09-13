554bbf161857002cf89daac07b833e8575641917
Tymur Bondar <tymurbondar@outlook.com>
2023-09-13 14:21:39 -0400

    adding todos to the list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementById('submitInput').onclick = function() {
+    event.preventDefault();
+    //create element
+    let inputField = document.getElementById("new-task-description");
+    let inputValue = inputField.value;
+    var newLi = document.createElement('li')
+    var node = document.createTextNode(inputValue);
+    newLi.appendChild(node);
+    document.getElementById('tasks').appendChild(newLi);
+    console.log("button clicked");
+    //
+  };
+  //add the users input to the newly created li element
 });
